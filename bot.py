0eadf0542f7510a753a4b5f34c54e3e92a9d1ec6
kaszczuj <juszczak.filip24@gmail.com>
2021-06-06 22:22:12 +0200

    main file fixed

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 import os
 from boto.s3.connection import S3Connection
 
-TOKEN = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+TOKEN = S3Connection(os.environ['TOKEN'], os.environ['TOKEN'])
 
 
 class MyClient(discord.Client):
